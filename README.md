4a19dbcfb1058f4513f10757e1b79002882a8ca3
noahtren <noahtren@gmail.com>
2019-01-29 10:39:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nutrient Object Management System (NOMS)
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="noms",
+    version="0.0.1",
+    author="Noah Trenaman",
+    author_email="hello@noahtrenaman.com",
+    description="Nutrient Object Management System (noms) using the USDA's FoodData Central",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/noahtren/noms",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
