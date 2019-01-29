b5b564d30938b5b4b0f26ea0bddd5933750a3ce4
noahtren <noahtren@gmail.com>
2019-01-29 10:51:34 -0500

    simple method for converting a dict of food ids and their mass to a food object or array of food objects

---
+++ noms/service/food.py
@@ -0,0 +1,18 @@
+from ..client.main import Client
+from ..client.dict_parse import food_parse
+from ..objects.nutrient_dict import nutrient_dict
+
+def foods(id_value_dict, client):
+    food_obj = client.food_query(id_value_dict.keys())
+    # Return list of food objects in context of nutrients being tracked
+    food_obj = food_parse(food_obj, nutrient_dict, list(id_value_dict.values()))
+    return food_obj
+
+def meal(foods):
+    meal = None
+    for food in foods:
+        if meal == None:
+            meal = food
+        else:
+            meal += food
+    return meal
