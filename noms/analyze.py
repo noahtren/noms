9996c50d18bc7bbec29deed9794e03eae4149f0b
noahtren <noahtren@gmail.com>
2019-04-22 00:34:45 -0400

    generalized loss function, added removal recommendation

--- noms/analyze.py
+++ noms/analyze.py
@@ -1,4 +1,5 @@
 from scipy.optimize import minimize
+from .objects.food import Food, Meal
 import copy 
 import sys
 import os
@@ -12,16 +13,17 @@ def norm_rda_deficit(norm_rda_arr):
         r_nut[ni]['value'] = 1 - r_nut[ni]['value']
     return r_nut
 
-def assess_deficit(meal, nutrient_dict):
-    print("Deficit breakdown of meal:")
+def loss(meal, nutrient_dict, verbose=False):
+    if verbose:
+        print("Deficit breakdown of meal:")
     deficit = norm_rda_deficit(meal.norm_rda(nutrient_dict))
     loss = 0; ni = 0
     for nut in deficit:
-        #if nut['value'] != 0:
-        print("{nut:<20}: {val:>10} percent unmet".format(nut=nut['name'], val=round(nut['value'] * 100, 1)))
+        if verbose:
+            print("{nut:<20}: {val:>10} percent unmet".format(nut=nut['name'], val=round(nut['value'] * 100, 1)))
         loss += nut['value'] ** 2
         ni += 1
-    print("Loss: ", round(loss, 2))
+    return loss
 
 def best_contributors(k, meal, suggestion, nutrient_dict, x):
     """
@@ -68,7 +70,6 @@ def suggestion_loss(meal, suggestion, nutrient_dict):
         return loss
     required_normed_nutrients = norm_rda_deficit(meal.norm_rda(nutrient_dict))
     sol = minimize(scaled_loss, 1, args=(required_normed_nutrients, suggestion, nutrient_dict), bounds=[(0.05, sys.maxsize)], tol=1e-2)
-    """
     # this can be uncommented to display a graph showing the convergence to minimize loss
     # as the mass of the given food is scaled
     import matplotlib.pyplot as plt
@@ -79,7 +80,6 @@ def suggestion_loss(meal, suggestion, nutrient_dict):
         losses.append(scaled_loss(inp, required_normed_nutrients, suggestion, nutrient_dict))
     plt.plot(xs, losses)
     plt.show()
-    """
     return (scaled_loss(sol.x[0], required_normed_nutrients, suggestion, nutrient_dict), sol.x[0])
 
 def generate_recommendations(meal, pantry, nutrient_dict, n, verbose=False):
@@ -108,3 +108,17 @@ def generate_recommendations(meal, pantry, nutrient_dict, n, verbose=False):
             print("^" * 50)
     rec_data.sort(key=lambda x: x[0])
     return rec_data[:n]
+
+def recommend_removal(meal, nutrient_dict):
+    assert type(meal) == Meal
+    o_loss = loss(meal, nutrient_dict) # calculate the original loss
+    losses = []
+    for i in range(0, len(meal.foods)):
+        # compute the loss for the meal without each food
+        this_meal = Meal([food for j, food in enumerate(meal.foods) if j != i])
+        losses.append(loss(this_meal, nutrient_dict))
+    if min(losses) < o_loss: 
+        return losses.index(min(losses)) # removing a certain food is beneficial
+    else:
+        return -1 # removing any certain food is detrimental
+    
